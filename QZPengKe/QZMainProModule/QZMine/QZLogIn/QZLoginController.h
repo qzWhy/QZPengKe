381ac806a975682524ad9fad98c9f28ccb29e1f3
qzWhy <745680110@qq.com>
2018-01-12 14:02:07 +0800

    1.12test

---
+++ QZPengKe/QZMainProModule/QZMine/QZLogIn/QZLoginController.h
@@ -0,0 +1,13 @@
+//
+//  QZLoginController.h
+//  QZPengKe
+//
+//  Created by 000 on 18/1/9.
+//  Copyright © 2018年 XiaoZuoXiaoYou. All rights reserved.
+//
+
+#import "QZBaseViewController.h"
+
+@interface QZLoginController : QZBaseViewController
+
+@end
---
+++ QZPengKe/QZMainProModule/QZMine/QZLogIn/QZLoginController.m
@@ -0,0 +1,26 @@
+//
+//  QZLoginController.m
+//  QZPengKe
+//
+//  Created by 000 on 18/1/9.
+//  Copyright © 2018年 XiaoZuoXiaoYou. All rights reserved.
+//
+
+#import "QZLoginController.h"
+
+@interface QZLoginController ()
+@property (weak, nonatomic) IBOutlet UIButton *sinaLoginBtn;
+@property (weak, nonatomic) IBOutlet UIButton *weChatBtn;
+
+@end
+
+@implementation QZLoginController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+
+
+@end
